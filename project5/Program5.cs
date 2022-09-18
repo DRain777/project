835f65e3709c5b730634c1cd77ce855197698e4b
Denis <d.cochew@yandex.ru>
2022-09-18 12:08:45 +0700

    основной код с username

---
+++ project5/Program5.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Ира")
+{
+   Console.WriteLine("Ира Красавица");
+}  
+else
+{
+    Console.Write("Здрасти");
+    Console.WriteLine(username);
+}
+
